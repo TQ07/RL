493bbe4c4d204b00f3dd3625d4966a306bea2f68
TQ07 <tql@mit.edu>
2024-12-08 22:28:23 -0500

    all logistics finished, just trying optimizing value and policy networks to not be stupid

--- main.py
+++ main.py
@@ -83,6 +83,8 @@ class Agent:
     
         '''
         probs = self.policy_network((state['pos'][0], state['pos'][1], int(state["has_reached_goal"])))
+        #print('model output probs', probs)
+
         action = torch.argmax(probs.detach()) + 1 # get the action from index + 1
 
         return action, torch.log(max(probs))
@@ -109,20 +111,22 @@ class Environment:
 
     ''' 
     1, 3: up and down
-    2, 4: left and right
+    2, 4: right and left
 
     pos y + action % 2
     '''
     def update_env(self, action):
         if action == 1:
-            self.y += 1
-        elif action == 2:
-            self.x += 1
-        elif action == 3:
-            self.y -= 1
-        elif action == 4:
             self.x -= 1
+        elif action == 2:
+            self.y += 1
+        elif action == 3:
+            self.x += 1
+        elif action == 4:
+            self.y -= 1
         
+        self.state['pos'] = (self.x, self.y) # integers are immutable in py, so when you update x and y, the x and y in state doesn't update
+
         if self.state['pos'] == self.goal:
             self.state['has_reached_goal'] = True
 
@@ -130,24 +134,37 @@ class Environment:
         self.map[prev_state['pos'][0]][prev_state['pos'][1]] = 0 
         self.map[state['pos'][0]][state['pos'][1]] = 'A'
 
+
     def is_move_legal(self, action, state):
+
+        #print(type(action))
         if state['pos'][0] == 0 and action == 1:
             return False
-        if state['pos'][0] == 9 and action == 3:
+        elif state['pos'][0] == 9 and action == 3:
+            #print('What?????????????????????????????????')
             return False
-        if state['pos'][1] == 0 and action == 4:
+        elif state['pos'][1] == 0 and action == 4:
             return False
-        if state['pos'][1] == 9 and action == 2:
+        elif state['pos'][1] == 9 and action == 2:
+            #print('What?????????????????????????????????')
             return False
+        else:
+            return True 
         
-    def reward(self, state, next_state):
+    def reward(self, prev_state, state):
         '''
         computes reward at time step t
         '''
-        init_dist = self.distance(state["pos"][0], state["pos"][1], self.goal[0], self.goal[1])
-        next_dist = self.distance(next_state["pos"][0], next_state["pos"][1], self.goal[0], self.goal[1])
+       # print('prev_state:', prev_state)
+        #print('state:', state)
 
-        return init_dist - next_dist
+        init_dist = self.distance(prev_state["pos"][0], prev_state["pos"][1], self.goal[0], self.goal[1])
+        next_dist = self.distance(state["pos"][0], state["pos"][1], self.goal[0], self.goal[1])
+
+        #print('init_dist:', init_dist)
+        #print('next_dist:', next_dist)
+
+        return (init_dist - next_dist) * 10
 
     def distance(self, x1, y1, x2, y2):
         return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
@@ -187,21 +204,23 @@ class Game:
 
     def calc_adv(self, rewards_to_go, expected_return_list):
         rewards_to_go = torch.as_tensor(rewards_to_go)
-        print(expected_return_list)
-        
+        expected_return_list = expected_return_list.detach() # remember to detach to aviod updating value network parameters
+
+        #print('r to go shape:', rewards_to_go.shape)
+        #print('expected return shape:', expected_return_list.shape)
+
         return rewards_to_go-expected_return_list
     
     def calc_value_loss(self, expected_return, reward):
 
-        print(expected_return)
+        #print(expected_return)
         reward = torch.as_tensor(reward)
         
         MSE = torch.pow(expected_return - reward, 2)
         return MSE
        
     def game_loop(self, update_interval):
-    
-        prev_state = self.env.state
+        prev_state = self.env.state.copy() # remember dicts are mutable; when you change one, the other changes 
         finish_reward = 0
 
         trajectory = []
@@ -209,32 +228,43 @@ class Game:
         expected_return_list = []
 
         for t in range(update_interval): 
+            penalty = -10
             print(f'Running game... Time step {self.env.time}\nState = {self.env.state}')
+
+            if self.env.time == 10:
+                raise Exception('stopppppppppp')
 
             # collect a set of trajectories D by running policy 
             action, prob = self.agent.policy(prev_state)
-
+            action = action.item()
             print('action:',action, ' prob:', prob)
 
             expected_return = self.agent.value(prev_state)
             expected_return_list.append(expected_return)
 
-            if (self.env.is_move_legal(action, prev_state)):
-                self.env.update_env(action)
+            print(prev_state)
+
+            if self.env.is_move_legal(action, prev_state):
+                self.env.update_env(action) # updates the state 
+                penalty = 0
+                #print('updated state:', self.env.state)
+            else:
+                print('Agent moving into wall! AHHHHHHHHHHHHHHHHHHHH')
+              
             trajectory.append((prev_state, action, prob))
+
+            if (self.env.state['pos'] == self.env.goal):
+                finish_reward = 100
+
+            reward = self.env.reward(prev_state, self.env.state)
+            print('reward:', reward)
+            reward_list.append(reward + finish_reward + penalty)
 
             self.env.update_map(prev_state, self.env.state)
             self.env.render()
 
-            # log the rewards
-            if (self.env.state['pos'] == self.env.goal):
-                finish_reward = 10 
-
-            reward_list.append(self.env.reward(prev_state, self.env.state) + finish_reward)
-            
-            prev_state = self.env.state
+            prev_state = self.env.state.copy()
             self.env.time += 1
-
             if (self.env.reached_goal):
                 break
 
@@ -251,7 +281,10 @@ class Game:
         self.reward_histroy.append(reward_list)
         self.expected_return_history.append(expected_return_list)
 
+        # some reshaping
         expected_return_list = torch.stack(expected_return_list)
+        expected_return_list = expected_return_list.squeeze()
+
         advantage = self.calc_adv(self.calc_rewards_to_go(update_interval, reward_list), expected_return_list)
 
         # print(expected_return_list)
@@ -259,18 +292,25 @@ class Game:
         #print(advantage)
         action_probs = torch.stack([t[2] for t in trajectory])
         #print(action_probs)
+
+        #print('advantage:', advantage)
+
         policy_loss_vector = advantage * action_probs
         policy_loss_avg = torch.mean(policy_loss_vector)
         policy_loss_avg.backward()
         self.policy_optimizer.step()
 
-        #print(policy_loss_avg)
+        #print('policy loss shape:', policy_loss_vector.shape)
+        print('policy loss avg:', policy_loss_avg)
 
         value_loss_vector = self.calc_value_loss(expected_return_list, reward_list)
+
+        #print('value loss shape:', value_loss_vector.shape)
+
         value_loss_avg = torch.mean(value_loss_vector)
 
 
-        print(value_loss_avg)
+        print('value loss avg:', value_loss_avg)
         value_loss_avg.backward()
         self.value_optimizer.step()
 
--- test.py
+++ test.py
@@ -1,63 +1,7 @@
-from torch import nn
-import torch
+import torch 
+import math
 
-class Policy(nn.Module):
-    '''
-    neural network that gives an action given a state
-    '''
-    def __init__(self):
-        super().__init__()
-        self.MLP = nn.Sequential(
-            nn.Linear(in_features=3, out_features=64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, 4),
-        )
-
-        self.head = nn.Softmax()
-
-    def forward(self, x):
-        x = torch.tensor(x)
-        x = x.to(torch.float32)
-        logits = self.MLP(x)
-        return self.head(logits)
-
-class Value(nn.Module):
-    '''
-    neural network that estimates the expected return/reward at a given state
-    '''
-    def __init__(self):
-        super().__init__()
-        self.MLP = nn.Sequential(
-            nn.Linear(in_features=3, out_features=32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32, 1),
-        )
-
-    def forward(self, x):
-        x = torch.tensor(x)
-        x = x.to(torch.float32)
-        logits = self.MLP(x)
-        return logits
-    
-'''policy = Policy()
-input = (0,0,0)
-output = max(policy(input))
-
-print(output)
-
-output.retain_grad()
-output.backward()
-
-params = nn.utils.parameters_to_vector(policy.parameters())
-grad = nn.utils.parameters_to_vector(p.grad for p in policy.parameters())
-
-
-print(grad)'''
-value = Value()
-input = [(0,0,0),(0,0,0),(0,0,0)]
-output = value(input)
-print(output)
+a = 1
+b = a
+a = 3
+print(b)
